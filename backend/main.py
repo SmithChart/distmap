d7fb0be3afecf1c2a8fea92889f1c4fcfc8bcb91
Chris Fiege <chris@tinyhost.de>
2018-11-24 15:43:30 +0100

    fixup! Backend: Add some more API draft
    Signed-off-by: Chris Fiege <chris@tinyhost.de>

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import Response
 import json
 
 app = Flask(__name__)
@@ -11,8 +12,11 @@ def root(lat, lon, options):
     r["center"] = {"lat":lat, "lon":lon}
     r["options"] = options
     r["pixlength"] = 10
-    pixmap = [(x,x,x*1.3) for x in range(0, 100)]
-    r["pixmap"] = pixmap
-    return json.dumps(r)
+    pixlist = []
+    for x in range(-2,2):
+        for y in range(-2,2):
+            pixlist.append((x, y, (x+y)*1.3))
+    r["pixlist"] = pixlist
+    return Response(json.dumps(r), mimetype='application/json')
 
 app.run(host='0.0.0.0', port=8080)
