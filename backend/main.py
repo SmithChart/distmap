61623efcc38234d51d3dfe4d6acb86a775037444
Chris Fiege <chris@tinyhost.de>
2018-11-24 15:31:41 +0100

    Backend: Add some more API draft
    Signed-off-by: Chris Fiege <chris@tinyhost.de>

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/distance/<lat>/<lon>/<options>")
 def root(lat, lon, options):
-    return "{}, {}, {}".format(lat, lon, options)
+    r = {}
+    r["center"] = {"lat":lat, "lon":lon}
+    r["options"] = options
+    r["pixlength"] = 10
+    pixmap = [(x,x,x*1.3) for x in range(0, 100)]
+    r["pixmap"] = pixmap
+    return json.dumps(r)
 
 app.run(host='0.0.0.0', port=8080)
