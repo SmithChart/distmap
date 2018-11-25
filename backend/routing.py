7220777ea10e96dd2898b87b19a8ba02d01fcd82
Stefan Hanisch <s.hanisch@brainplug.de>
2018-11-25 10:59:17 +0100

    sorting removed

--- backend/routing.py
+++ backend/routing.py
@@ -1,6 +1,5 @@
 import pyroutelib3_file as pyr
 import math
-import numpy as np
 import os
 
 class Generator(object):
@@ -8,7 +7,7 @@ class Generator(object):
     def __init__(self, osm_path = '/home/stefan/Downloads/map (2)', speed = 0.833, vehicle = 'car'):
         self.name = "Generator"
         self.osm_path = osm_path
-        self.router = pyr.Router(vehicle)#, osm_path)
+        self.router = pyr.Router(vehicle, osm_path)
         self.speed = speed
 
     def distanceInKmBetweenEarthCoordinates(self, lat1, lon1, lat2, lon2):
@@ -39,6 +38,7 @@ class Generator(object):
             end = self.router.findNode(pixel['lat'], pixel['lon'])
             status, route = self.router.doRoute(start, end) # Find the route - a list of OSM nodes
             if status == 'success':
+                print ('start duration')
                 routeLatLons = list(map(self.router.nodeLatLon, route))
                 dist = 0
                 for i in range(len(routeLatLons) - 1):
@@ -56,29 +56,8 @@ class Generator(object):
         if skipped > 0:
             print("Skipped {} points while routing.".format(skipped))
 
-
-        # sort by angle
-        list_to_sort = list()
-        for pix in pixList:
-            dot_product = pix['lat'] * lat + pix['lon'] * lon
-            len_p1 = math.sqrt(pix['lat']**2 + pix['lon']**2)
-            len_p2 = math.sqrt(lat**2 + lon**2)
-            list_to_sort.append((pix['lat'], pix['lon'], pix['t'],math.acos(dot_product / (len_p1 * len_p2))))
-
-        dtype = [('lat', float), ('lon', float), ('t', float), ('angle', float)]
-        array_to_sort = np.array(list_to_sort, dtype)
-        array_to_sort.sort(order=['lat', 'lon'])
-
-        print(array_to_sort)
-
-        list_to_return = list()
-        for tuple in array_to_sort:
-            list_to_return.append({tuple[0], tuple[1], tuple[2]})
-
-
-        return list_to_return
-        #return pixList
+        return pixList
 
 #
-gen = Generator()
-print(gen.calc(52.268725, 10.510546, [{'lat': 52.267041, 'lon': 10.514387}, {'lat':52, 'lon': 10}]))
+# gen = Generator()
+# print(gen.calc(52.268725, 10.510546, [{'lat': 52.267041, 'lon': 10.514387}, {'lat':52, 'lon': 10}]))
