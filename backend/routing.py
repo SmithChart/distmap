b6338b761ca17b9e556b90bf42d2821530acac5a
Stefan Hanisch <s.hanisch@brainplug.de>
2018-11-25 11:53:45 +0100

    pool

--- backend/routing.py
+++ backend/routing.py
@@ -11,7 +11,7 @@ def route_calculation(input_list):
         routeLatLons = list(map(router.nodeLatLon, route))
         dist = 0
         for i in range(len(routeLatLons) - 1):
-            dist += self.distanceInKmBetweenEarthCoordinates(routeLatLons[i][0], routeLatLons[i][1], routeLatLons[i+1][0], routeLatLons[i+1][1])
+            dist += distanceInKmBetweenEarthCoordinates(routeLatLons[i][0], routeLatLons[i][1], routeLatLons[i+1][0], routeLatLons[i+1][1])
 
         duration = dist * 1000 / speed
 
@@ -20,30 +20,33 @@ def route_calculation(input_list):
             'lon': pixel['lon'],
             't': duration
         }
+    return None
+
+def distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2):
+    def degreesToRadians(degrees):
+      return degrees * math.pi / 180
+
+    earthRadiusKm = 6378
+
+    dLat = degreesToRadians(lat2-lat1)
+    dLon = degreesToRadians(lon2-lon1)
+
+    lat1 = degreesToRadians(lat1)
+    lat2 = degreesToRadians(lat2)
+
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return earthRadiusKm * c
 
 class Generator(object):
     """docstring for Generator."""
     def __init__(self, osm_path = '/home/stefan/Downloads/map (2)', speed = 0.833, vehicle = 'car'):
         self.name = "Generator"
         self.osm_path = osm_path
-        self.router = pyr.Router(vehicle)#), osm_path)
+        self.router = pyr.Router(vehicle), osm_path)
         self.speed = speed
 
-    def distanceInKmBetweenEarthCoordinates(self, lat1, lon1, lat2, lon2):
-        def degreesToRadians(degrees):
-          return degrees * math.pi / 180
 
-        earthRadiusKm = 6371
-
-        dLat = degreesToRadians(lat2-lat1)
-        dLon = degreesToRadians(lon2-lon1)
-
-        lat1 = degreesToRadians(lat1)
-        lat2 = degreesToRadians(lat2)
-
-        a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        return earthRadiusKm * c
 
     def calc(self,lat, lon, pixels):
         skipped = 0
@@ -85,5 +88,5 @@ class Generator(object):
         #return pixList
 
 #
-gen = Generator()
-print(gen.calc(52.268725, 10.510546, [{'lat': 52.267041, 'lon': 10.514387}, {'lat':52, 'lon': 10}]))
+# gen = Generator()
+# print(gen.calc(52.268725, 10.510546, [{'lat': 52.267041, 'lon': 10.514387}, {'lat':52, 'lon': 10}]))
